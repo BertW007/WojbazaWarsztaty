10e02b8c4e827902e6c25c736ad9718a4c312d6d
Wojtek <wojtek0126@gmail.com>
2018-08-31 15:22:41 +0200

    Frankoscene1

---
+++ facefee/__init__.py
@@ -0,0 +1,36 @@
+def chapter1_info():
+    return """
+                                               Franko the story reborn
+                                                  Epizod 1: Wpierd...
+     Scena1:    
+     
+     Miejsce:           
+               Osiedle, podworko miedzy blokami, poczatek lat 90tych.
+     
+     Czas i pora roku:
+              Lato, 13:00
+     
+     Postacie fabularne: 
+               -mlody Franek,
+               -mlody Alex,
+               -konfidziad Alojzy Ucho.
+     Statysci:
+               -baba jadwiga z jamnikiem
+               -osiedlowy bej Ryszard
+               -dzieci grajace w pilke          
+               
+     Opis sceny: 
+               Franek i Alex umowili sie na wyprawe do sklepu komputerowego
+               po 'IBMA'- najnowszy komputer, ktorym zamierzaja wlamac sie do 
+               bazy Klocura, ojca Klocka - osiedlowego oprycha.
+               Celem wlamu jest wyzerowanie dlugow, ktore rodzice Alexa i Franka musieli
+               zaciagnac na nieuczciwych zasadach od Klocucha.
+               Chlopaki za butelki i puszki(i miedz z kabli kolejowych) uzbierali
+               na IBMA. dzis wagaruja.
+               Wyprawa do sklepu niekoniecznie nalezy do latwych.
+               Przebiega bowiem przez alejke w ktorej jest lokalna speluna, a przed nia
+               czesto rezyduje Klocek ze swoja banda.
+               Alex i Franex nie wiedza, ze ich rozmowe potajemnie podsluchuje najwiekszy osiedlowy konfident,
+               Alojzy Ucho...
+               """
+
